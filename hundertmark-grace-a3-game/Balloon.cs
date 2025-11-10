ccc116d5abb377ee1702ac21270c303020011d17
grace-h2004 <gracehundertmark@yaho.ca>
2025-11-10 13:02:59 -0500

    balloon gravity partial working in class

--- hundertmark-grace-a3-game/Balloon.cs
+++ hundertmark-grace-a3-game/Balloon.cs
@@ -11,10 +11,57 @@ namespace MohawkGame2D
 {
     public class Balloon
     {
-       
+        //variables
+        public Vector2 position;
+        public Vector2 velocity;
+        public float radius = 35;
+
+        Color balloonColor = new Color(191, 3, 3);
+
+        public Balloon() { }
+
+        public Balloon(Vector2 position) 
+        {  
+            this.position = position; 
+        }
+
+        
 
 
-            
+        public void DrawBalloon()
+        {
+            // Set up variables once game is ready
+            position = new(Window.Width / 4, 50);
+           
+            // Run game logic
+            ApplyGravity();
+            ConstrainBalloonToScreen();
+
+            //gen draw config
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 1;
+
+            //draw balloon
+            Draw.FillColor = balloonColor;
+            Draw.Ellipse(position.X, position.Y, 60, 60);
+
+            void ApplyGravity()
+            {
+                velocity += new Vector2(0, 10) * Time.DeltaTime;
+                position += velocity;
+            }
+            void ConstrainBalloonToScreen()
+            {
+                if (position.Y + radius >= Window.Height)
+                {
+                    velocity.Y = -velocity.Y;
+                    velocity *= 0.8f;
+                    position.Y = Window.Height - radius;
+                }
+            }
+        }
+        
+
 
 
     }
--- hundertmark-grace-a3-game/Game.cs
+++ hundertmark-grace-a3-game/Game.cs
@@ -14,11 +14,9 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
-        Vector2 position;
-        Vector2 velocity;
-        float radius = 35;
+        Balloon balloon = new Balloon();
 
-        Color balloonColor = new Color(191, 3, 3);
+
         Color sky = new Color(0, 250, 255);
         
 
@@ -30,9 +28,6 @@ namespace MohawkGame2D
             Window.SetTitle("Balloon Dodge");
             Window.SetSize(800, 600);
 
-            // Set up variables once game is ready
-            position = new(Window.Width / 4, 50);
-         
         }
 
         /// <summary>
@@ -43,40 +38,10 @@ namespace MohawkGame2D
             // Reset background
             Window.ClearBackground(sky);
 
-            // Run game logic
-            ApplyGravity();
-            ConstrainBalloonToScreen();
+            balloon.DrawBalloon();
 
-            //gen draw config
-            Draw.LineColor = Color.Black;
-            Draw.LineSize = 1;   
-
-            //draw balloon
-            Draw.FillColor = balloonColor;
-            Draw.Ellipse(position.X, position.Y, 60, 60);
-            
         }
-        void ApplyGravity()
-        {
-            // Apply gravity to velocity
-            velocity += new Vector2(0, 10) * Time.DeltaTime;
-            // Apply velocity to position
-            position += velocity;
-        }
-
-        void ConstrainBalloonToScreen()
-        {
-            // Is bottom of ball at or past edge of screen
-            if (position.Y + radius >= Window.Height)
-            {
-                // Inverse Y velocity, move upwards
-                velocity.Y = -velocity.Y;
-                // Slow down velocity
-                velocity *= 0.8f; // retain 80% of velocity
-                // Place ball against bottom edge of screen
-                position.Y = Window.Height - radius;
-            }
-        }
+        
     }
 
 }
